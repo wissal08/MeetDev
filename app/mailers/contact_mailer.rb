d4685925f2fb143f5e37b0952ed12dc309cc774b
sonia <wissal.jrad@gmail.com>
2016-05-07 12:00:19 +0000

    modified mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 
 class ContactMailer < ActionMailer::Base
-  default to: 'wissal.jrad@email_address.com'
+  default to: 'wissal.jrad@gmail.com'
   
   def contact_email(name, email, body)
     @name = name
