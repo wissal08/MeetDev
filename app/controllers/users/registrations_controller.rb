aee1af0f2eba2d0e493004083aa1cac5b882ca85
sonia <wissal.jrad@gmail.com>
2016-06-16 17:37:57 +0000

    Updated registration file

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     def create
         super do |resource|
             if params[:plan]
-                resource.plan__id=params[:plan]
+                resource.plan_id=params[:plan]
                 if resource.plan_id==2
                     resource.save_with_payment
                 else
