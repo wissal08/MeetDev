38b01a6c8dfb8beaf0b21a2829d51feeb4caa6a1
sonia <wissal.jrad@gmail.com>
2016-11-21 11:31:14 +0000

    updating actionmailer

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -21,4 +21,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
        redirect_to root_url
        end
     end
+    def sign_up_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    def account_update_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+    end     
 end
--- config/application.rb
+++ config/application.rb
@@ -20,4 +20,4 @@ module MeetDev
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   end
-end
+# SMTP settings for gmail
