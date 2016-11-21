003a4e4485e7876aab176f128be02cb981611bea
sonia <wissal.jrad@gmail.com>
2016-11-21 11:59:21 +0000

    updated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-config.action_mailer.default_url_options = { host: 'secure-sands-36271.herokuapp.com'}
+config.action_mailer.default_url_options = { host: 'https://secure-sands-36271.herokuapp.com'}
 config.action_mailer.delivery_method = :smtp
 end
